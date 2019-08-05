8cab2fc5a78e02e0e2f5716aa91615a9062c88d0
Alex Mollard <alex.mollard@mail.com>
2019-08-05 14:47:02 +1000

    First Commit

---
+++ PixelArt/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelArt
+{
+	public partial class PixelEdit : Form
+	{
+		public PixelEdit()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Console.WriteLine("Wassup");
+		}
+
+		private void pictureBox5_Click(object sender, EventArgs e) // NewFile
+		{
+			
+		}
+
+		private void pictureBox4_Click(object sender, EventArgs e) // OpenFile
+		{
+			OpenFileDialog openFile = new OpenFileDialog();
+
+			openFile.Filter = "PixelArt|*.png;*.jpg";
+			openFile.Title = "Pixel Loader";
+
+			if (openFile.ShowDialog() == DialogResult.OK)
+			{
+				string fileName = openFile.FileName;
+				Bitmap myImage = new Bitmap(fileName);
+
+				pictureBox1.Image = myImage;
+
+			}
+			
+
+			//pictureBox1.BackgroundImage = ;
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
